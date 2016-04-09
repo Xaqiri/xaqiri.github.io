86bc7f18f9d174f0d90baa62df672f74391173f2
Zachary Robbins <xaqiri@outlook.com>
2016-04-09 13:49:09 -0500

    Implemented OWM calls to get location, temp, and weather conditions.  Icons TBA.

--- Weather/main.js
+++ Weather/main.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
     var loc = [];
+    var cityName = "";
+    var country = "";
+    var temp = 0;
+    var conditions = "";
     var apiKey = "8a5910e9d87b9b06f733d4de4979b649";
+    var things = "";
+    var weatherIcons = ["wi-day-sunny", "wi-night-clear", "wi-cloudy", "wi-raindrops", "wi-snowflake-cold", "wi-storm-showers"];
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            loc[0] = position.coords.latitude;
-            loc[1] = position.coords.longitude;
-            $.getJSON("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID="+apiKey, function(a) {
+            $.getJSON("http://api.openweathermap.org/data/2.5/forecast/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&units=imperial" + "&APPID=" + apiKey, function(a) {
+                cityName = a.city.name;
+                country = a.city.country;
+                temp = a.list[0].main.temp;
+                conditions = a.list[0].weather[0].main;
+                $("#location").html(cityName + ", " + country);
+                $("#tempNum").html(temp + " ");
+                $("#conditions").html(conditions);
             });
         });
     }
+    $("#tempMode").mouseover(function() {
+        $(this).css("color", "#FFF");
+    });
+    $("#tempMode").mouseout(function() {
+        $(this).css("color", "#AAA");
+    });
 });
