c0cbc1f4e00ffb4d1af00f972671ac96abfda35e
Zachary Robbins <xaqiri@outlook.com>
2016-04-09 12:23:25 -0500

    Design mostly finished, working on OpenWeatherMap api calls

--- Weather/main.js
+++ Weather/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-    
+    var loc = [];
+    var apiKey = "8a5910e9d87b9b06f733d4de4979b649";
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            loc[0] = position.coords.latitude;
+            loc[1] = position.coords.longitude;
+            $.getJSON("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID="+apiKey, function(a) {
+            });
+        });
+    }
 });
