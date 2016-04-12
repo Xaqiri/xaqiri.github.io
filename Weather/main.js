b02f65ca0bec56330c543825961a5f2dea75c4a9
Zachary Robbins <xaqiri@outlook.com>
2016-04-11 19:18:14 -0500

    Colors and icons now change based on time of day

--- Weather/main.js
+++ Weather/main.js
@@ -1,9 +1,17 @@
+// Colors: [bgColor, titleColor, textColor]
+var dayColors = ["#e3cd3c", "#6b0000", "#a23e3e"];
+var nightColors = ["#080C14", "#AADDDD", "#BBBBCC"];
+var d = new Date();
+var h = d.getHours();
+var timeOfDay = "";
+
 var mo = function(id) {
     $(id).mouseover(function() {
         $(this).css("color", "#FFF");
     });
     $(id).mouseout(function() {
-        $(this).css("color", "#AAA");
+        if (timeOfDay === "day") { $(this).css("color", dayColors[2]); }
+        else { $(this).css("color", nightColors[2]); }
     });
 };
 
@@ -17,7 +25,8 @@ var getIcon = function(id) {
     var icon = 0;
     if (id >= 200 && id < 600) { icon = 3; }
     else if (id >= 600 && id < 700) { icon = 4; }
-    else if (id === 800) { icon = 0; }
+    else if (id === 800 && timeOfDay === "day") { icon = 0; }
+    else if (id === 800 && timeOfDay === "night") { icon = 1; }
     else if (id > 800 && id < 900) { icon = 2; }
     else { id = 5; }
     return weatherIcons[icon];
@@ -35,6 +44,21 @@ var tempConversion = function(temp) {
     return newTemp;
 };
 
+var getTime = function() {
+    if (h <= 6 || h >=19) {
+        $("body").css("background-color", nightColors[0]);
+        $("#title").css("color", nightColors[1]);
+        $("body").css("color", nightColors[2]);
+        timeOfDay = "night";
+    } else {
+        $("body").css("background-color", dayColors[0]);
+        $("#title").css("color", dayColors[1]);
+        $("body").css("color", dayColors[2]);
+        timeOfDay = "day";
+    }
+    return timeOfDay;
+};
+
 $(document).ready(function() {
     var loc = [];
     var cityName = "";
@@ -44,6 +68,7 @@ $(document).ready(function() {
     var apiKey = "8a5910e9d87b9b06f733d4de4979b649";
     var conditionIcon = "";
     var conID = 0;
+    var timeOfDay = getTime();
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             $.getJSON("http://api.openweathermap.org/data/2.5/forecast/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&units=imperial" + "&APPID=" + apiKey, function(a) {
